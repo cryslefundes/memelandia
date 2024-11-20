da8a188ed230275382c0dfdc10c81c102ba6c0c7
crystian lefundes <crystian.lf@gmail.com>
2024-11-19 22:49:30 -0300

    feat: Add cliente feign de usuario no servico de categoria

---
+++ categoriaService/src/main/java/br/com/ebac/categorias/client/UsuarioClientFeign.java
@@ -0,0 +1,16 @@
+package br.com.ebac.categorias.client;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.UUID;
+
+@Configuration
+@EnableDiscoveryClient
+@FeignClient(name = "usuarios", url = "http://usuario-service:8081/usuarios")
+public interface UsuarioClientFeign {
+        @GetMapping("/{id}/is-cadastrado")
+        Boolean encontrarUsuarioPorId(@PathVariable UUID id);
+}
